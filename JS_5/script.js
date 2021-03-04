8ba680cebc576f6c3c8e84f3696027ca95490835
unknown <luca.grabherr@student.htldornbirn.at>
2021-03-04 16:28:32 +0100

    Onlinerechner

---
+++ JS_5/script.js
@@ -0,0 +1,23 @@
+function buttonclicked(){
+    console.log("Done");
+
+    let num1 = document.getElementById("number1").value;
+    let num2 = document.getElementById("number2").value;
+
+    
+    
+}
+
+buttonclicked();
+function selection(){
+    console.log("selection");
+}
+
+function Plus(){
+    console.log("Plus");
+    let num1 =document.getElementById("Plus")
+    let result = parseFloat(num1) + parseFloat(num2);
+    console.log(result);
+
+}
+
